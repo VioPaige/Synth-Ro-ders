f071d671807af710da20a519420649d1bc825572
VioPaige <85783108+VioPaige@users.noreply.github.com>
2021-11-21 15:56:38 +0100

    Update noteScript.lua

--- noteScript.lua
+++ noteScript.lua
@@ -1,35 +1,50 @@
 -- Services
 local rStorage = game:GetService("ReplicatedStorage")
 
-
+wait(.01)
 -- Instances
 	-- Folders
 local remotesFolder = rStorage:WaitForChild("Remotes")
 	-- Other
 local addPointsRemote = remotesFolder:WaitForChild("Addpoints")
+local addStreakRemote = remotesFolder:WaitForChild("Addstreak")
 
-
-
+local summoningplayer = game.Players[script.Parent.Player.Value]
+local touched = false
 
 
 -- Functions
 function addPoints(plr, amount)
 
 	print("Adding " .. amount .. " points to " .. plr.Name)
-
+	
+	touched = true
 	plr.leaderstats.points.Value +=amount
+	plr.leaderstats.streak.Value +=1
+	addPointsRemote:FireClient(plr, plr.leaderstats.points.Value)
+	addStreakRemote:FireClient(plr, plr.leaderstats.streak.Value)
+	script.Parent:Destroy()
 
 end
 
 
 function touched(otherpart)
 
-	print("Note has been touched (wow pedophile)")
+	--print("Note has been touched (wow pedophile)")
+	print("Touched by " .. otherpart.Name)
+	print("and is " .. script.Parent.Name)
 
-	if otherpart.Parent:FindFirstChild("Humanoid") and (otherpart.Name == "RightHand" or otherpart.Name == "LeftHand") then
+	if otherpart.Parent:FindFirstChild("Humanoid") and otherpart.Name == script.Parent.Name and otherpart.Parent.Name == summoningplayer.Name then
 
 		noteDown(otherpart, script.Parent.Position, otherpart.Position)
-
+		
+	elseif otherpart == workspace.NoteDestructor then
+		
+		summoningplayer.leaderstats.points.Value = 0
+		addStreakRemote:FireClient(summoningplayer, 0)
+		
+		script.Parent:Destroy()
+		
 	end
 
 end
@@ -44,13 +59,19 @@ function noteDown(otherpart, notepos, otherpartpos)
 	local mag = (notepos - otherpartpos).Magnitude
 	
 	print(mag)
-
-	if mag < 5 then
+	
+	if mag < 4.2 then
+		
+		addPoints(game.Players[otherpart.Parent.Name], 1000)
+		
+	elseif mag < 5 then
 
 		addPoints(game.Players[otherpart.Parent.Name], 500)
-		wait(.05)
-		script.Parent:Destroy()
-
+		
+	elseif mag < 7 then
+		
+		addPoints(game.Players[otherpart.Parent.Name], 100)
+	
 	end
 
 end
@@ -59,3 +80,5 @@ end
 
 -- Events
 script.Parent.Touched:Connect(touched)
+--script.Parent.Destroying:Connect(onDestruction)
+
