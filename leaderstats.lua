241514cdfbc2c88cef6ae70ef631b3d60beec522
VioPaige <85783108+VioPaige@users.noreply.github.com>
2021-11-21 12:39:03 +0100

    Create leaderstats.lua

---
+++ leaderstats.lua
@@ -0,0 +1,59 @@
+-- Services
+local Players = game:GetService("Players")
+local rStorage = game:GetService("ReplicatedStorage")
+
+
+
+-- Instances
+	-- Folders
+local remotesFolder = rStorage:WaitForChild("Remotes")
+	-- Other
+local addPointsRemote = remotesFolder:WaitForChild("Addpoints")
+local serverPrintRemote = remotesFolder:WaitForChild("serverPrint")
+
+
+
+
+
+
+-- Functions
+function initPlr(plr)
+
+	local ls = Instance.new("Folder")
+	ls.Name = 'leaderstats'
+	ls.Parent = plr
+
+	local points = Instance.new("IntValue")
+	points.Name = 'points'
+	points.Value = 0
+	points.Parent = ls
+
+end
+
+
+
+function addPoints(plr, amount)
+	
+	print("Adding " .. amount .. " points to " .. plr.Name)
+	
+	plr.leaderstats.points.Value +=amount
+	
+end
+
+
+function serverPrint(plr, message)
+	
+	print("(" .. plr.Name .. ") - " .. message)
+	
+end
+
+
+
+
+
+
+-- Events
+Players.PlayerAdded:Connect(initPlr)
+
+addPointsRemote.OnServerEvent:Connect(addPoints)
+serverPrintRemote.OnServerEvent:Connect(serverPrint)
